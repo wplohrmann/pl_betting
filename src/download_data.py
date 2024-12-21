bb55967a6169be672539176d328b17f99ff520f6
William Lohrmann <will@hoxtonfarms.com>
2024-12-21 21:05:01 +0000

    Get data from the last 10 years

--- src/download_data.py
+++ src/download_data.py
@@ -1,12 +1,12 @@
 import requests
 
 
-def main(oldest_year: int):
-    for year in range(oldest_year, 2024):
+def main(oldest_year: int) -> None:
+    for year in range(oldest_year, 2025):
         download_data(year)
 
 
-def download_data(year: int):
+def download_data(year: int) -> None:
     url = f"https://www.football-data.co.uk/mmz4281/{year - 2000}{year - 1999}/E0.csv"
     response = requests.get(url)
     if response.status_code == 200:
@@ -14,3 +14,6 @@ def download_data(year: int):
             file.write(response.content)
     else:
         print(f"Failed to download data for year {year}")
+
+if __name__ == "__main__":
+    main(2014)
